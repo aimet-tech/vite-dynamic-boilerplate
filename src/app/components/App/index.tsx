7df2ecea9c0a29309156bfa305f53ae9702efa90
Veerin Phana-ngam <beam7684@gmail.com>
2024-04-23 13:01:09 +0700

    fix: Remove console log

--- src/app/components/App/index.tsx
+++ src/app/components/App/index.tsx
@@ -3,7 +3,7 @@ import { RouterProvider } from 'react-router-dom'
 import createLazyRouter from 'router/utils/createLazyRouter'
 
 const router = createLazyRouter(appRoutes)
-console.log('asdasd')
+
 function App() {
   return <RouterProvider router={router} />
 }
